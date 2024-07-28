da50fe69a5ad5a56c39f8120314c676439025801
Rohini0518 <rohinipolina0518@gmail.com>
2024-07-28 18:28:38 +0530

    updated two if conditions to one

---
+++ oop-coffee-machine-start/main.py
@@ -0,0 +1,27 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+coffee_maker=CoffeeMaker()
+money_machine=MoneyMachine()
+menu=Menu()
+
+is_on=True
+
+while is_on:
+  print(menu.get_items())
+  choice=input("choose your favourite coffee amoung above menu: ")
+  if choice=="report" :
+     coffee_maker.report()
+     money_machine.report()
+  elif choice=="off":
+     is_on=False  
+  else:
+     drink=menu.find_drink(choice)
+     cost=drink.cost
+     if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(cost):
+         coffee_maker.make_coffee(drink)
+     else:
+        is_on=False   
+  
